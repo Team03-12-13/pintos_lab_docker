3587df71f19b61c7b640c3dde7b5aa956b28e7ef
이윤아 <qwer120542@gmail.com>
2025-05-28 14:34:59 +0900

    all pass 확인

---
+++ pintos/include/filesys/filesys.h
@@ -0,0 +1,24 @@
+#ifndef FILESYS_FILESYS_H
+#define FILESYS_FILESYS_H
+
+#include <stdbool.h>
+#include "filesys/off_t.h"
+#include "threads/synch.h"        // 🔒 lock 구조체 사용을 위한 헤더 추가
+
+/* Sectors of system file inodes. */
+#define FREE_MAP_SECTOR 0       /* Free map file inode sector. */
+#define ROOT_DIR_SECTOR 1       /* Root directory file inode sector. */
+
+/* Disk used for file system. */
+extern struct disk *filesys_disk;
+
+/* Global lock for file system access. */
+extern struct lock filesys_lock;  // 🔒 파일 시스템 락 외부 선언
+
+void filesys_init (bool format);
+void filesys_done (void);
+bool filesys_create (const char *name, off_t initial_size);
+struct file *filesys_open (const char *name);
+bool filesys_remove (const char *name);
+
+#endif /* filesys/filesys.h */
---
+++ pintos/include/userprog/syscall.h
@@ -0,0 +1,28 @@
+#ifndef USERPROG_SYSCALL_H
+#define USERPROG_SYSCALL_H
+
+#include <stdbool.h>
+#include "threads/interrupt.h"
+#include "threads/init.h"
+
+typedef int pid_t;
+
+void syscall_init (void);
+
+void halt (void);
+void exit(int status);
+pid_t fork (const char *thread_name, struct intr_frame *if_);
+int exec (const char *cmd_line);
+int wait (pid_t pid);
+bool create (const char *file, unsigned initial_size);
+bool remove (const char *file);
+int open (const char *filename);
+int filesize (int fd);
+int read (int fd, void *buffer, unsigned size);
+int write (int fd, const void *buffer, unsigned size);
+void seek (int fd, unsigned position);
+unsigned tell (int fd);
+void close (int fd);
+bool check_address(const void *addr);
+
+#endif /* userprog/syscall.h */
